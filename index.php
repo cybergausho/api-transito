941cb80d9099343163e980f2d9e0fb99916ac9e6
anonred <anonred@anonred>
2020-08-12 03:58:41 -0300

    primer marcador

--- index.php
+++ index.php
@@ -32,8 +32,10 @@ $cortes = json_decode($json, true); //decodificar el json a array
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cortes CABA</title>
 
-    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css" integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ==" crossorigin="" /> <!-- estilos del mapa -->
 
+    <script type="text/javascript" src="https://maps.google.com/maps/api/js?sensor=false"></script>
+    <script type="text/javascript" src="https://code.jquery.com/jquery-1.8.2.js"></script>
+    <script type="text/javascript" src="https://www.ign.gob.ar/argenmap/argenmap.jquery.min.js"></script>
 </head>
 
 <body>
@@ -52,26 +54,51 @@ $cortes = json_decode($json, true); //decodificar el json a array
             //echo "<br>".$location."<br>";
             //   var_dump($cortes["incidents"][$i]["location"]["polyline"]);
 
-            $ubicacion = explode(" ", $location);  //corto el string por espacios - separa latitud y longitud (tmb en api se repite tres veces seguidas c/u)
+            $ubicacion = explode(" ", $location);  //corto el string por espacios - separa latitud y longitud 
             $latitud = $ubicacion[0];
             $longitud = $ubicacion[1];
             echo "Ubicacion del corte: <br> Latitud" . $latitud . "<br> Longitud" . $longitud . "<br>";
-            //echo "<iframe src=https://www.google.com/maps/search/?api=1&query=".$latitud.",".$longitud.'width="600" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>'; 
-            //aca arriba quise saltearme el uso de la api porque google-maps me pide cobro para su uso - pero me bloqueo 
 
+    ?>
+            <script>
+                //     lat: <?php //echo $latitud ?>,
+                //     lng: <?php //echo $longitud ?>,
+                //     contenido: "<?php //echo $cortes["incidents"][$i]["description"] ?>"
+          </script>
+    <?php
 
         } else {
             echo "No detalla ubicacion";
         }
-
         echo "<br><br>";
     }
 
     ?>
     <script>
-        var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+
     </script>
+    <div id="mapa" style="height:315px;"></div>
 
 </body>
+<script type="text/javascript">
+    $(document).ready(function() {
+        var marcador = [
+            <?php echo $latitud ?>,
+            <?php echo $longitud ?>
+        ]
+        console.log(marcador)
+        console.log(marcador[1])
+        
+        $("#mapa").argenmap()
+        $("#mapa").centro(-34.63, -58.52).zoom(13)
+        $("#mapa").agregarMarcador(marcador)
+
+
+
+    });
+
+
+
+</script>
 
 </html>
